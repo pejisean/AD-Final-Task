3252a1f6bd982fedebe963dc3e071115384f8af2
Jay <janjayjil@gmail.com>
2025-06-27 21:46:14 +0800

    feat (index.php): add missing Google Fonts stylesheet link for consistent typography

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="assets/css/headfoot.css" />
     <link rel="stylesheet" href="assets/css/home.css" />
     <script src="assets/js/script.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
