be929f9ed8c886e7237f119d15add8ac8624adb0
Jay <janjayjil@gmail.com>
2025-06-29 01:56:06 +0800

    feat(pages): update page titles and add preloader to enhance user experience

--- pages/other.php
+++ pages/other.php
@@ -4,16 +4,19 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>The Last Trade Post - Other Essentials</title>
+    <title>The Last Trade Post - Electronics & Power</title>
     <link rel="stylesheet" href="assets/css/headfoot.css" />
+    <link rel="stylesheet" href="assets/css/loader.css" />
     <link rel="stylesheet" href="assets/css/trading/trading.css" />
     <script src="assets/js/script.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
-
 <body>
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
     <div class="sticky-header">
         <header>
             <div class="logo">
