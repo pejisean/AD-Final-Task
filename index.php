060fd6dbde5620f1cce7a1204f44b132c6eb1a24
Jay <janjayjil@gmail.com>
2025-07-01 16:56:23 +0800

    fix (index.php): correct stylesheet links in index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>The Last Trade Post - Home</title>
-    <link rel="stylesheet" href="assets/css/headfoot.css" />
+    <link rel="stylesheet" href="assets/css/global.css" />
+    <link rel="stylesheet" href="assets/css/header.css" />
+    <link rel="stylesheet" href="assets/css/footer.css" />
     <link rel="stylesheet" href="assets/css/home.css" />
     <link rel="stylesheet" href="assets/css/loader.css" />
     <script src="assets/js/script.js"></script>
