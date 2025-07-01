fba15c2ee06285e1391a69aa214f3347a4e85fbd
Jay <janjayjil@gmail.com>
2025-07-01 17:53:28 +0800

    fix (military.php): update page title and modify navigation links for military grade clarity

--- pages/military.php
+++ pages/military.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>The Last Trade Post - Electronics & Power</title>
+    <title>The Last Trade Post - Military Grade</title>
     <link rel="stylesheet" href="assets/css/header.css"/>
     <link rel="stylesheet" href="assets/css/footer.css"/>
     <link rel="stylesheet" href="assets/css/global.css"/>
@@ -12,14 +12,15 @@
     <link rel="stylesheet" href="assets/css/loader.css"/>
     <link rel="stylesheet" href="assets/css/shop/trading.css"/>
     <script src="assets/js/script.js"></script>
-    <script src="assets/js/marketplace.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
     <!-- Header & Navigation -->
     <div class="sticky-header">
         <header>
@@ -29,9 +30,10 @@
 
             <div class="header-right">
                 <nav class="main-nav">
-                    <a href="electronics.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Electronics & Power</a>
-                    <a href="tools.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Tools & Equipment</a>
-                    <a href="weapons.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Weapons & Defense</a>
+                    <a href="marketplace.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Marketplace</a>
+                    <a href="electronics.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Electronics</a>
+                    <a href="tools.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Tools</a>
+                    <a href="weapons.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Weapons</a>
                     <a href="other.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Other Essentials</a>
                     <a href="military.php" style="user-select: none; -webkit-user-select: none; -moz-user-select: none;  -ms-user-select: none; cursor: pointer;">Military Grade</a>
                 </nav>
