d5df6565c90df46941eb9bf1ae57271dc8d55890
Jay <janjayjil@gmail.com>
2025-07-01 18:18:33 +0800

    fix (marketplace.php): simplify navigation links by removing unnecessary text from menu items

--- pages/marketplace.php
+++ pages/marketplace.php
@@ -27,20 +27,16 @@
             <div class="logo">
                 <a href="index.php"><img src="assets/img/HomeLogo.png" alt="The Last Trade Post Logo"></a>
             </div>
-
             <div class="header-right">
                 <nav class="main-nav">
                     <a href="pages/marketplace.php"
                         style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Marketplace</a>
                     <a href="pages/electronics.php"
-                        style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Electronics
-                        & Power</a>
+                        style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Electronics</a>
                     <a href="pages/tools.php"
-                        style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Tools
-                        & Equipment</a>
+                        style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Tools</a>
                     <a href="pages/weapons.php"
-                        style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Weapons
-                        & Defense</a>
+                        style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Weapons</a>
                     <a href="pages/other.php"
                         style="user-select: none; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; cursor: pointer;">Other
                         Essentials</a>
