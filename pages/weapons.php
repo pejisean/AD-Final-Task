8103e1e990a46626bc655cb62eec1621d79a153a
Jay <janjayjil@gmail.com>
2025-07-01 17:07:16 +0800

    fix (weapons.php): update title and stylesheet links for consistency with other pages

--- pages/weapons.php
+++ pages/weapons.php
@@ -4,8 +4,11 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>The Last Trade Post - Weapons & Defense</title>
-    <link rel="stylesheet" href="assets/css/headfoot.css" />
+    <title>The Last Trade Post - Electronics & Power</title>
+    <link rel="stylesheet" href="assets/css/header.css" />
+    <link rel="stylesheet" href="assets/css/footer.css" />
+    <link rel="stylesheet" href="assets/css/global.css" />
+    <link rel="stylesheet" href="assets/css/responsive.css" />
     <link rel="stylesheet" href="assets/css/loader.css" />
     <link rel="stylesheet" href="assets/css/trading/trading.css" />
     <script src="assets/js/script.js"></script>
@@ -13,7 +16,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://fonts.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
-
 <body>
     <div id="preloader">
         <div class="loader"></div>
