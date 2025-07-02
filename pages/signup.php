4a863f1786efd62c71a8bcc6c4c84f298aca368f
Jay <janjayjil@gmail.com>
2025-07-02 23:27:47 +0800

    fix (signup.php): remove redundant navigation links from header for improved usability

--- pages/signup.php
+++ pages/signup.php
@@ -29,13 +29,6 @@
                 <a href="../index.php"><img src="assets/img/HomeLogo.png" draggable="false" alt="The Last Trade Post Logo"></a>
             </div>
             <div class="header-right">
-                <nav class="main-nav">
-                    <a href="electronics.php">Electronics & Power</a>
-                    <a href="tools.php">Tools & Equipment</a>
-                    <a href="weapons.php">Weapons & Defense</a>
-                    <a href="other.php">Other Essentials</a>
-                    <a href="military.php">Military Grade</a>
-                </nav>
                 <div class="hamburger" onclick="toggleMenu()">☰</div>
             </div>
             <div class="dropdown-menu" id="dropdownMenu">
