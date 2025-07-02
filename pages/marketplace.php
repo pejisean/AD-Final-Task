3079fc50496b1ddbf821c3e0ce1405d1a50a5310
Jay <janjayjil@gmail.com>
2025-07-02 23:05:11 +0800

    fix (marketplace.php): update login/signup link to point to login.php

--- pages/marketplace.php
+++ pages/marketplace.php
@@ -48,7 +48,7 @@
             </div>
 
             <div class="dropdown-menu" id="dropdownMenu">
-                <a href="#">👤 Login / Sign Up</a>
+                <a id="login-signup-link" href="login.php">👤 Login / Sign Up</a>
                 <a href="/index.php">🏠 Home</a>
                 <a href="#">🛒 Cart (0)</a>
                 <a href="#">📄 History</a>
