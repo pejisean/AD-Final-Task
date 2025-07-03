58018b12e48d1f9a30f3cde369654531de704456
pejisean <202311735@fit.edu.ph>
2025-07-03 22:02:48 +0800

    feat - added logic to envSetter.util.php

---
+++ utils/envSetter.util.php
@@ -0,0 +1,14 @@
+<?php
+    require_once BASE_PATH . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
+    $dotenv->load();
+
+    // Distribute the data using array key
+    $pgConfig = [
+        'pg_host'   => $_ENV['PG_HOST'] ?? null,
+        'pg_port'   => $_ENV['PG_PORT'] ?? null,
+        'pg_db'     => $_ENV['PG_DB'] ?? null,
+        'pg_user'   => $_ENV['PG_USER'] ?? null,
+        'pg_pass'   => $_ENV['PG_PASS'] ?? null,
+    ];
+?>
