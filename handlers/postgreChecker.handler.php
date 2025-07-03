21308f30a27dedf1bd50a32d97ad1e4ae778f522
pejisean <202311735@fit.edu.ph>
2025-07-04 05:46:26 +0800

    fix - fixed DB credentials

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,10 +1,10 @@
 <?php
 require_once UTILS_PATH . '/envSetter.util.php';
 $host = getenv('PG_HOST') ?: 'host.docker.internal';
-$port = getenv('PG_PORT') ?: '5555';
+$port = getenv('PG_PORT') ?: '5112';
 $username = getenv('PG_USER') ?: 'user';
 $password = getenv('PG_PASS') ?: 'password';
-$dbname = getenv('PG_DB') ?: 'weatherdatabase';
+$dbname = getenv('PG_DB') ?: 'thelasttradepost_db';
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
