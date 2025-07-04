fce6e1e85069bb9e5f8efd13f7f87c578dbf90f2
pejisean <202311735@fit.edu.ph>
2025-07-04 15:55:03 +0800

    feat (user.staticData): add function to return static user data

---
+++ staticDatas/user.staticData.php
@@ -0,0 +1,11 @@
+<?php
+function getUsersData() {
+    return [
+        [
+            'username' => 'john.smith',
+            'email' => 'john.smith@example.com',
+            'password' => 'p@ssW0rd1234',
+            'role' => 'user'
+        ]
+    ];
+}
