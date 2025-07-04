249369d5a0641684fcb1909ee206568aae731569
pejisean <202311735@fit.edu.ph>
2025-07-04 19:12:03 +0800

    fix (bootstrap.php): add missing trailing slashes to path definitions

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
-define('UTILS_PATH', BASE_PATH.'/utils');
-define('HANDLERS_PATH', BASE_PATH.'/handlers');
-define('DUMMIES_PATH', BASE_PATH.'/staticDatas');
+define('UTILS_PATH', BASE_PATH.'/utils/');
+define('HANDLERS_PATH', BASE_PATH.'/handlers/');
+define('DUMMIES_PATH', BASE_PATH.'/staticDatas/');
 
 
 
