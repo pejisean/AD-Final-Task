5c4fd6ea1898c4f6fd6c66822cee1c8b9fcd74fb
Jay <janjayjil@gmail.com>
2025-07-11 16:14:51 +0800

    refactor (script.component.php, history.php, marketplace.php, other.php, tools.php, weapons.php, global.css) Implement PHP components and fix asset paths

--- pages/history.php
+++ pages/history.php
@@ -2,22 +2,9 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>The Last Trade Post - Purchase History</title>
-    <link rel="stylesheet" href="assets/css/global.css" />
-    <link rel="stylesheet" href="assets/css/header.css" />
-    <link rel="stylesheet" href="assets/css/footer.css" />
+    <?php require_once '../components/head.component.php';?>
     <link rel="stylesheet" href="assets/css/shop/history.css" />
-    <link rel="stylesheet" href="assets/css/loader.css" />
-    <link rel="stylesheet" href="assets/css/feedback.css" />
-    <script src="assets/js/script.js"></script>
-    <script src="assets/js/login.js"></script>
-    <script src="assets/js/signup.js"></script>
-    <script src="assets/js/history.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
