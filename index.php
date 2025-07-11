8d8c5729ef0edfc1714c417d0d88dec7732402b1
Jay <janjayjil@gmail.com>
2025-07-11 15:27:31 +0800

    refactor (index.php, head.component.php): Implement PHP components and fix asset paths, removed example/template components

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
-    <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en" class="hide-scrollbar">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>The Last Trade Post - Home</title>
-    <link rel="stylesheet" href="assets/css/global.css" />
-    <link rel="stylesheet" href="assets/css/header.css" />
-    <link rel="stylesheet" href="assets/css/footer.css" />
-    <link rel="stylesheet" href="assets/css/home.css" />
-    <link rel="stylesheet" href="assets/css/loader.css" />
-    <link rel="stylesheet" href="assets/css/feedback.css" />
-    <script src="assets/js/script.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
+<?php require_once 'components/head.component.php'; ?>
 </head>
 
 <body>
