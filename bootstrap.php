1a875714f6c3be6edb298bee9ecbd7027fef0651
pejisean <202311735@fit.edu.ph>
2025-07-13 03:30:07 +0800

    fix (bootstrap): ensure session starts only if not already active

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,10 @@ define('UTILS_PATH', BASE_PATH.'/utils/');
 define('HANDLERS_PATH', BASE_PATH.'/handlers/');
 define('DUMMIES_PATH', BASE_PATH.'/staticDatas/');
 
-
+//Start session upon startup
+if (session_status() == PHP_SESSION_NONE){
+    session_start();
+}
 
 
 
