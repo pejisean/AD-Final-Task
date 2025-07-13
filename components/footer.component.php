487655f563e1d18fa64ded874cf2aa4c26ec3565
MadmanMigz <202310011@fit.edu.ph>
2025-07-14 00:09:41 +0800

    refactor (components): updated pathway to fix LINKS error in footer.

--- components/footer.component.php
+++ components/footer.component.php
@@ -7,18 +7,18 @@
         <div class="footer-links-section">
             <h4>Links</h4>
             <a href="../index.php">Home</a>
-            <a href="marketplace.php">Marketplace</a>
-            <a href="electronics.php">Electronics</a>
-            <a href="tools.php">Tools</a>
-            <a href="weapons.php">Weapons</a>
-            <a href="other.php">Other Essentials</a>
+            <a href="pages/marketplace.php">Marketplace</a>
+            <a href="pages/electronics.php">Electronics</a>
+            <a href="pages/tools.php">Tools</a>
+            <a href="pages/weapons.php">Weapons</a>
+            <a href="pages/other.php">Other Essentials</a>
         </div>
 
         <div class="footer-company-section">
             <h4>Company</h4>
-            <a href="about.php">About Us</a>
-            <a href="contact.php">Contact Us</a>
-            <a href="faq.php">FAQs</a>
+            <a href="pages/about.php">About Us</a>
+            <a href="pages/contact.php">Contact Us</a>
+            <a href="pages/faq.php">FAQs</a>
         </div>
     </div>
 
