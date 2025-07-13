f472fe6babbfccd3ce7205e9f055c2f85176a89c
MadmanMigz <202310011@fit.edu.ph>
2025-07-13 19:10:10 +0800

    refactor (footer): updated the whole footer.

--- components/footer.component.php
+++ components/footer.component.php
@@ -1,17 +1,28 @@
 <footer>
-    <div>
-        <h4>Links</h4>
-        <a href="../index.php">Home</a>
-        <a href="marketplace.php">Marketplace</a>
-        <a href="electronics.php">Electronics</a>
-        <a href="tools.php">Tools</a>
-        <a href="weapons.php">Weapons</a>
-        <a href="other.php">Other Essentials</a>
+    <div class="footer-content">
+        <div class="footer-logo-section">
+            <img src="../assets/img/HomeLogo.png" alt="The Last Trade Post Logo" class="footer-logo">
+        </div>
+
+        <div class="footer-links-section">
+            <h4>Links</h4>
+            <a href="../index.php">Home</a>
+            <a href="marketplace.php">Marketplace</a>
+            <a href="electronics.php">Electronics</a>
+            <a href="tools.php">Tools</a>
+            <a href="weapons.php">Weapons</a>
+            <a href="other.php">Other Essentials</a>
+        </div>
+
+        <div class="footer-company-section">
+            <h4>Company</h4>
+            <a href="about.php">About Us</a>
+            <a href="contact.php">Contact Us</a>
+            <a href="faq.php">FAQs</a>
+        </div>
     </div>
-    <div>
-        <h4>Company</h4>
-        <a href="about.php">About Us</a>
-        <a href="#">Contact Us</a>
-        <a href="#">FAQs</a>
+
+    <div class="footer-bottom-message">
+        <p>© 2025 The Last Trade Post. All rights reserved.</p>
     </div>
 </footer>
