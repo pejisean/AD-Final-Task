f507a4ab1b75431a87ab2c651583e1ab975b0a7d
pejisean <202311735@fit.edu.ph>
2025-07-13 15:18:57 +0800

    fix (login): add PHP session check to redirect logged-in users to the homepage

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,11 @@
+<?php
+require_once '../bootstrap.php';
+require_once UTILS_PATH . 'auth.util.php';
+    if (AuthUtil::isLoggedIn()) {
+        header("Location: ../index.php");
+        exit();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en" class="hide-scrollbar">
 
