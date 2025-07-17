78ab52da665bf22f67201b3b3621b90d2eb51a44
pejisean <202311735@fit.edu.ph>
2025-07-17 08:45:23 +0800

    fix(script): remove marketplace.js script inclusion from header

--- components/script.component.php
+++ components/script.component.php
@@ -2,7 +2,6 @@
     <script src="assets/js/script.js"></script>
     <script src="assets/js/login.js"></script>
     <script src="assets/js/history.js"></script>
-    <script src="assets/js/marketplace.js"></script>
     <script src="assets/js/signup.js"></script>
     <script src="assets/js/buynowoverlay.js"></script>
     <script src="assets/js/addtocartoverlay.js"></script>
